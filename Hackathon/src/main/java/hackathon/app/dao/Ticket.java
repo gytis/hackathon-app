e7d9c627a982f674bc1b282765adca5964d65fd6
mxs888 <smaxnov@gmail.com>
2015-06-13 23:57:56 +0100

    Rating and ticket

---
+++ Hackathon/src/main/java/hackathon/app/dao/Ticket.java
@@ -0,0 +1,58 @@
+package hackathon.app.dao;
+
+import android.text.format.Time;
+
+import java.util.Date;
+
+/**
+ * Created by Maxim on 13/06/2015.
+ */
+public class Ticket {
+    private long id;
+    private long userId;
+    private long eventId;
+    private Date date;
+    private Date createdAt;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+
+}

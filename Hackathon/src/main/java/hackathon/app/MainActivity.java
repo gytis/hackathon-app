d575a5a9c0821f6d9df2f84796d7520cc321d3c9
gytis <gytis@redhat.com>
2015-06-13 17:59:00 +0100

    Check if registered and redirect

--- Hackathon/src/main/java/hackathon/app/MainActivity.java
+++ Hackathon/src/main/java/hackathon/app/MainActivity.java
@@ -16,43 +16,52 @@ public class MainActivity extends Activity {
 
     private CallbackManager callbackManager;
 
+    private Intent eventsActivityIntent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        callbackManager = CallbackManager.Factory.create();
-        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
-
-        final Intent eventsActivityIntent = new Intent(this, EventsActivity.class);
-
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.v("Facebook Login", "success");
-                startActivity(eventsActivityIntent);
-
-                // TODO check if registered
-                // TODO register
-                // TODO upload friends
-                // TODO redirect
-            }
-
-            @Override
-            public void onCancel() {
-                Log.v("Facebook Login", "cancel");
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                Log.v("Facebook Login", exception.getMessage());
-            }
-        });
+        eventsActivityIntent = new Intent(this, EventsActivity.class);
 
         if (AccessToken.getCurrentAccessToken() != null) {
+            Log.v(this.getClass().getName(), "Facebook token exists. Redirecting");
             startActivity(eventsActivityIntent);
+        } else {
+            Log.v(this.getClass().getName(), "Facebook token does not exist. Need to log in");
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, new FacebookLoginCallback());
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Log.v(this.getClass().getName(), "Facebook token exists. Redirecting");
+            startActivity(eventsActivityIntent);
+        } else {
+            Log.v(this.getClass().getName(), "Facebook token does not exist. Need to log in");
+        }
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Log.v(this.getClass().getName(), "Facebook token exists. Redirecting");
+            startActivity(eventsActivityIntent);
+        } else {
+            Log.v(this.getClass().getName(), "Facebook token does not exist. Need to log in");
         }
     }
 
@@ -60,6 +69,30 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class FacebookLoginCallback implements FacebookCallback<LoginResult> {
+
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Log.v("Facebook Login", "success");
+            startActivity(eventsActivityIntent);
+
+            // TODO check if registered
+            // TODO register
+            // TODO upload friends
+            // TODO redirect
+        }
+
+        @Override
+        public void onCancel() {
+            Log.v("Facebook Login", "cancel");
+        }
+
+        @Override
+        public void onError(FacebookException exception) {
+            Log.v("Facebook Login", exception.getMessage());
+        }
     }
 
 }
